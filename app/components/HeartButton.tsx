e13b6a3330141428c1331a58f2c8a32e707c8fbf
Nigel <nigeltakadza@gmail.com>
2023-09-02 11:58:54 +0200

    this need to be revisted

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,21 +1,25 @@
-"use client";
-import React from "react";
-import { SafeUser } from "../types";
+'use client';
+
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
- 
+import useFavorite from "@/app/hooks/useFavorite";
+import { SafeUser } from "@/app/types";
 
-interface HeartButtonProps{
-  listingId: string;
-  curentUser?: SafeUser | null
+
+
+interface HeartButtonProps {
+  listingId: string
+  currentUser?: SafeUser | null
 }
-const HeartButton: React.FC<HeartButtonProps> = ({
-  listingId, curentUser
-})  => {
 
-  const hasFavorited = false;
-  const toggleFavorite = () => {};
-
+const HeartButton: React.FC<HeartButtonProps> = ({ 
+  listingId,
+  currentUser
+}) => {
+  const { hasFavorited, toggleFavorite } = useFavorite({
+    listingId,
+    currentUser
+  });
 
   return (
     <div 
